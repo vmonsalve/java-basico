7b114ddba8e819a033590098dcab729bf523a33b
Vicente Monsalve <vicente.monsalve@outlook.com>
2016-10-12 00:33:13 -0300

    Constantes y operadores I

---
+++ Constantes.java
@@ -0,0 +1,57 @@
+/*
+ * Ejercicios relacionados con constantes, 
+ * variables y operadores.
+ * */
+public class Constantes {
+	public static void main(String[] args) {
+		
+		/*
+		 * Primer ejercicio instanciacion y asignacion
+		 * de variables y operadores. 
+		 * 
+		 * double a = 5;
+		 * double b;
+		 * b = 7;
+		 * double c = b/a;
+		 *		 
+		 * division de a mas b, si a es igual 5 y b es 
+		 * igual a 7 como resultado nos deberia dar 1,4
+		 * pero como a y b son enteros nos ignora la 
+		 * parte decimal y se queda con la parte entera.
+		 * 
+		 * int c = b/a; imprime 1
+		 * 
+		 * Para que esto no ocurra debemos cambiar el tipo
+		 * de datoa double.
+		 * 
+		 * double c = b/a; imprime 1.4
+		 * 
+		 * Suma de valores enteros b+a
+		 * donde a es igual a 5 y b es igual a 7
+		 * 
+		 * int c=b+a;
+		 * 
+		 * Si c vale 12. 
+		 * 
+		 * c++; 
+		 * Con esto aumentara en uno el valor de c 
+		 * imprimiendo en pantalla 13.
+		 * 
+		 * c+=6;
+		 * Con esto le decimos que aumente en seis la
+		 * variable c con lo cual nos dara 18 como 
+		 * resultado.
+		 *
+		 * c-=6;
+		 * Al ejecutar esta instrucción la decimos que
+		 * le reste seis numeros a la variable c, con 
+		 * lo cual en pantalla nos debe mostrar el valor 
+		 * de 6.
+		 *
+		 */
+		int c = 5;
+		System.out.println(c);
+		
+	}
+
+}
