f25e48f62fc20222eeb57caa92af5bd5f6d1b1ce
Vicente Monsalve <vicente.monsalve@outlook.com>
2016-10-12 10:30:45 -0300

    Uso de clase Math y metodo sqrt

---
+++ ClaseMath.java
@@ -0,0 +1,20 @@
+
+public class ClaseMath {
+
+	public static void main(String[] args) {
+		/*
+		 * Ejercicios para aprender a usar la clase
+		 * Math.
+		 * 
+		 * Acontinuación usaremos el metodo sqrt que
+		 * devuelve la raiz cuadrada de un numero, 
+		 * para ello debemos recibir el resultado en 
+		 * una variable tipo double, porque el metodo
+		 * nos devuelve un entero, y asi no tener 
+		 * errores en la compilación
+		 * */
+		double raiz = Math.sqrt(9.0);
+		System.out.println("La raiz cuadrada de 9 es: "+raiz);
+	}
+
+}
