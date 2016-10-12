55ad72cd84c4ce345f72e844f79a86791f4ea5b6
Vicente Monsalve <vicente.monsalve@outlook.com>
2016-10-11 23:38:55 -0300

    Segundo ejercicio en java: Tipos de datos.

---
+++ TiposDatos.java
@@ -0,0 +1,20 @@
+
+public class TiposDatos {
+	public static void main(String args[]){
+		//Instanciamos y declaramos la variable edad
+		byte edad = 31;
+		/*
+		 * Verificamos si la persona es mayor de edad
+		 * y mostramos un mensaje dependiendo su con-
+		 * dición de acuerdo a la edad.
+		 * 
+		 * */
+		if(edad <= 18){
+			System.out.println("Eres menor de edad y tienes: "+edad);
+		}else{
+			System.out.println("Eres mayor de edad y tienes: "+edad);
+		}
+		
+		
+	}
+}
