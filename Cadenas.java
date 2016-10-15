0fa6be315d424e1111ad3eac5b9a3e5fc8fca12d
Vicente Monsalve <vicente.monsalve@outlook.com>
2016-10-15 20:55:47 -0300

    Clase String probando algunos metodos

--- Cadenas.java
+++ Cadenas.java
@@ -1,8 +1,16 @@
 
 public class Cadenas {
 	public static void main(String[] args) {
+		String nombre_usuario = "vmonsalve";
+		String nombre_usuario_prueba = "Vmonsalve";
 		
-
+		/* Probando algunos metodos de la clase string. */
+		
+		System.out.println("Método length: "+nombre_usuario.length());
+		System.out.println("Método chartAt: "+nombre_usuario.charAt(3));
+		System.out.println("Método substring: "+nombre_usuario.substring(1, 4));
+		System.out.println("Método equals: "+nombre_usuario.equals(nombre_usuario_prueba));
+		System.out.println("Método equalsIgnoreCase: "+nombre_usuario.equalsIgnoreCase(nombre_usuario_prueba));
 	}
 
 }
